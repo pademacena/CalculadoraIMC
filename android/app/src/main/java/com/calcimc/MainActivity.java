1974f4b96d1539ab585cbc07b76ed5bbcc0ffa74
pauloaugustodemacena <pauloaugustodemacena@gmail.com>
2019-09-21 23:23:52 -0300

    Update MainActivity.java

--- android/app/src/main/java/com/calcimc/MainActivity.java
+++ android/app/src/main/java/com/calcimc/MainActivity.java
@@ -1,5 +1,9 @@
 package com.calcimc;
 
+import com.facebook.react.ReactActivityDelegate;
+import com.facebook.react.ReactRootView;
+import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
+
 import com.facebook.react.ReactActivity;
 
 public class MainActivity extends ReactActivity {
@@ -12,4 +16,5 @@ public class MainActivity extends ReactActivity {
     protected String getMainComponentName() {
         return "calcimc";
     }
+    
 }
